7b79ea2c3cf02bd0b69360fb3b97b23e9e478a73
devonzachary <dzz>
2017-06-03 04:59:52 -0700

    lighting tweaks

--- src/Game.py
+++ src/Game.py
@@ -3,10 +3,13 @@ from Newfoundland import Camera, Controllers, Player, Floor, BaseGame
 class Game( BaseGame ):
 
     def initialize(self):
-        self.camera         = self.create_tickable( Camera( p = [0.0,0.0], zoom = 0.5 ) )
+        self.camera         = self.create_tickable( Camera( p = [0.0,0.0], zoom = 0.3 ) )
         self.controllers    = self.create_tickable( Controllers() )
         self.player         = self.create_tickable( Player( controllers = self.controllers ) )
         self.floor          = self.create_tickable( Floor( width = 200, height = 200, camera = self.camera, player = self.player ) )
+
+        self.player.p = [ -4,-4 ]
+        self.camera.p = self.player.p
 
     def render(self):
         self.floor.render()
