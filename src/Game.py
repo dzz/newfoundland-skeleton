4df4754607b2e39c70ad1571add8e0e485becb1f
devonzachary <dzz>
2017-06-03 08:44:26 -0700

    gi experiments

--- src/Game.py
+++ src/Game.py
@@ -3,10 +3,10 @@ from Newfoundland import Camera, Controllers, Player, Floor, BaseGame
 class Game( BaseGame ):
 
     def initialize(self):
-        self.camera         = self.create_tickable( Camera( p = [0.0,0.0], zoom = 0.3 ) )
+        self.camera         = self.create_tickable( Camera( p = [0.0,0.0], zoom = 0.2 ) )
         self.controllers    = self.create_tickable( Controllers() )
         self.player         = self.create_tickable( Player( controllers = self.controllers ) )
-        self.floor          = self.create_tickable( Floor( width = 200, height = 200, camera = self.camera, player = self.player ) )
+        self.floor          = self.create_tickable( Floor( width = 64, height = 64, camera = self.camera, player = self.player ) )
 
         self.player.p = [ -4,-4 ]
         self.camera.p = self.player.p
