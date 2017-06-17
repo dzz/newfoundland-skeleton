00ac444768f30282f3f4fcd53d1d234ec75d1e3d
devonzachary <dzz>
2017-06-17 09:47:02 -0700

    refactor

--- src/Game.py
+++ src/Game.py
@@ -1,11 +1,15 @@
-from Newfoundland import Camera, Controllers, Player, Floor, BaseGame
+from Newfoundland.Camera import Camera
+from Newfoundland.Controllers import Controllers
+from Newfoundland.Player import Player
+from Newfoundland.Floor import Floor
+from Newfoundland.BaseGame import BaseGame
 
 class Game( BaseGame ):
 
     def initialize(self):
         self.camera         = self.create_tickable( Camera( p = [0.0,0.0], zoom = 0.25 ) )
         self.controllers    = self.create_tickable( Controllers() )
-        self.player         = self.create_tickable( Player( controllers = self.controllers ) )
+        self.player         = Player( controllers = self.controllers ) 
         self.floor          = self.create_tickable( Floor( width = 64, height = 64, camera = self.camera, player = self.player ) )
 
         self.player.p = [ -4,-4 ]
